2939b21e00e68a178bfe199770092f5371932ec0
Sree1Lekha <57091379+Sree1Lekha@users.noreply.github.com>
2023-03-28 15:40:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.49.0"
+    }
+  }
+}
+
+
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-    version = "-3.0.0"
-    features {}
+  features {}
 }
 
-resource "azurerm_resource_group" "example" {
-    name ="example"
-    location = "East US"
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "Terra_prac_rq"
+  location = "West Europe"
+}
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "vnet" {
+  name                = "Terra_Prac_vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
 }
 
-output "id" {
-    value = data.azurerm_resource_group.example.id
+resource "azurerm_subnet" "subnet" {
+  name                 = "Terra_Prac_subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
